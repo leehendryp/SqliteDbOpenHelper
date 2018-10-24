5d919ee011c541429a52f031c22e451bbd51a72f
leehendryp <leehendryp@gmail.com>
2018-10-23 21:23:11 -0300

    Initial commit

---
+++ app/src/main/java/br/com/digitalhouse/sqlitedbopenhelper/interfaces/RecyclerViewOnItemClickListener.java
@@ -0,0 +1,7 @@
+package br.com.digitalhouse.sqlitedbopenhelper.interfaces;
+
+import br.com.digitalhouse.sqlitedbopenhelper.model.Person;
+
+public interface RecyclerViewOnItemClickListener {
+    void onItemClick(Person person);
+}
